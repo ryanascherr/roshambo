788f82befbcb580ef5958c9dbdb6e6d5ce0ce835
ryanascherr <ryanascherr@gmail.com>
2021-10-17 16:55:14 -0400

    Added different emojis for different results.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const options = ['rock', 'paper', 'scissor'];
+const playerLossFaces = ['&#128539;', '&#128540;', '&#128527;', '&#128526;', '&#128514;'];
+const playerWinFaces = ['&#128561;', '&#128556;', '&#128555;', '&#128548;', '&#128544;', '&#128542;', '&#128531;', '&#128534;', '&#129301;'];
+const thinkFace = '&#129300;';
+const meanFace = '&#128520;';
+const tieFace = '&#128562;';
 let oppChoice;
 let playerChoice;
+let oppFace = $(".opp-face")
 
 $("img").click(function() {
     playerChoice = $(this).attr("data-type");
@@ -17,9 +23,14 @@ function randomizeOpponentChoice() {
 function decideWinner() {
     if (playerChoice == oppChoice) {
         $(".result").html("Tie!");
+        oppFace.html(tieFace);
     } else if ((playerChoice == "rock" && oppChoice == "scissor") || (playerChoice == "paper" && oppChoice == "rock") || (playerChoice == "scissor" && oppChoice == "paper")) {
         $(".result").html("You Win!");
+        let currentFace = playerWinFaces[Math.floor(Math.random()*playerWinFaces.length)];
+        oppFace.html(currentFace);
     } else {
         $(".result").html("You Lose!");
+        let currentFace = playerLossFaces[Math.floor(Math.random()*playerLossFaces.length)];
+        oppFace.html(currentFace);
     }
 }
