21cac2d1195f9231e3465d41a33d39791f449d5a
ryanascherr <ryanascherr@gmail.com>
2021-10-19 14:32:16 -0400

    Added intro animation and opponent names.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const options = ['rock', 'paper', 'scissor'];
 const playerLossFaces = ['&#128539;', '&#128540;', '&#128527;', '&#128526;', '&#128514;'];
 const playerWinFaces = ['&#128561;', '&#128556;', '&#128555;', '&#128548;', '&#128544;', '&#128542;', '&#128531;', '&#128534;', '&#129301;'];
+const evilNames = ['Zyler Razor', 'Sephiran Ash', 'Arc Maleficum', 'Karayan Vossen', 'Viscardi Lobo', 'Orfeo Chainsaw', 'Carmin Shadowend', 'Xander Zul', 'Zander Bloodworth', 'Emma Magnus', 'Fatima Stone', 'Klyn Hallewell', 'Diamanda Umbra', 'Norrix Lestat', 'Fane Storm', 'Gnash Grim', 'Athena Sephiran', 'Zola Fang', 'Tabitha Shadowend'];
 const thinkFace = '&#129300;';
 const meanFace = '&#128520;';
 const tieFace = '&#128562;';
+let oppName;
 let oppChoice;
 let playerChoice;
 let oppFace = $(".opp-face")
@@ -19,6 +21,10 @@ $('.test').click(function() {
     oppFace.html(thinkFace);
     oppFace.addClass('think-face');
 })
+
+oppName = evilNames[Math.floor(Math.random()*evilNames.length)];
+console.log(oppName);
+$(".opp-name").html(oppName);
 
 function randomizeOpponentChoice() {
     oppChoice = options[Math.floor(Math.random()*options.length)];
@@ -42,4 +48,46 @@ function decideWinner() {
         oppFace.removeClass('think-face');
     }
 }
+
+$(window).on('load', function() {
+    setTimeout(function () {
+        $(".one").removeClass("hidden");
+    }, 1000);
+    setTimeout(function () {
+        $(".two").removeClass("hidden");
+    }, 1500);
+    setTimeout(function () {
+        $(".three").removeClass("hidden");
+    }, 2000);
+    setTimeout(function () {
+        $(".four").removeClass("hidden");
+    }, 2500);
+    setTimeout(function () {
+        $(".five").removeClass("hidden");
+    }, 3000);
+    setTimeout(function () {
+        $(".six").removeClass("hidden");
+    }, 3250);
+    setTimeout(function () {
+        $(".seven").removeClass("hidden");
+    }, 3500);
+    setTimeout(function () {
+        $(".intro").addClass("hidden");
+    }, 4000);
+    setTimeout(function () {
+        $(".ro").removeClass("hidden");
+    }, 4000);
+    setTimeout(function () {
+        $(".sham").removeClass("hidden");
+    }, 4250);
+    setTimeout(function () {
+        $(".bo").removeClass("hidden");
+    }, 4500);
+    setTimeout(function () {
+        $(".ro").addClass("bob1");
+        $(".sham").addClass("bob3");
+        $(".bo").addClass("bob1");
+        $(".test2").slideToggle();
+    }, 4750);
+})
 
